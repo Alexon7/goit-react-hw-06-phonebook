5453b630893f98088570f1a25e349ef97a2ce0d2
Alexon7 <alenkaalexon@gmail.com>
2023-06-07 19:06:47 +0200

    fix index

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 
 import { GlobalStyleComponent } from '../../styles/GlobalStyles';
 
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
 
 import { ContactForm } from '../ContactForm/ContactForm';
 
@@ -11,52 +11,52 @@ import { ContactList } from '../ContactList/ContactList';
 import { MainTitle, Title, Container } from './App.styled';
 
 export const App = () => {
-  const [contacts, setContacts] = useState(
-    () => JSON.parse(localStorage.getItem('contacts')) ?? []
-  );
-  const [filter, setFilter] = useState('');
+  // const [contacts, setContacts] = useState(
+  //   () => JSON.parse(localStorage.getItem('contacts')) ?? []
+  // );
+  // const [filter, setFilter] = useState('');
 
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
+  // useEffect(() => {
+  //   localStorage.setItem('contacts', JSON.stringify(contacts));
+  // }, [contacts]);
 
-  const onFormSubmit = (name, number) => {
-    const contact = {
-      name,
-      number,
-      id: nanoid(),
-    };
-    const isAtList = contacts.find(contact => contact.name === name);
-    if (isAtList) {
-      alert('Already in list');
-      return;
-    }
+  // const onFormSubmit = (name, number) => {
+  //   const contact = {
+  //     name,
+  //     number,
+  //     id: nanoid(),
+  //   };
+  //   const isAtList = contacts.find(contact => contact.name === name);
+  //   if (isAtList) {
+  //     alert('Already in list');
+  //     return;
+  //   }
 
-    setContacts([...contacts, contact]);
-  };
+  //   setContacts([...contacts, contact]);
+  // };
 
-  const deleteContact = id => {
-    setContacts(contacts.filter(contact => contact.id !== id));
-  };
+  // const deleteContact = id => {
+  //   setContacts(contacts.filter(contact => contact.id !== id));
+  // };
 
-  const onInputChange = e => {
-    const { value } = e.target;
-    setFilter(value);
-  };
+  // const onInputChange = e => {
+  //   const { value } = e.target;
+  //   setFilter(value);
+  // };
 
-  const filteredContacts = () =>
-    contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
+  // const filteredContacts = () =>
+  //   contacts.filter(contact =>
+  //     contact.name.toLowerCase().includes(filter.toLowerCase())
+  //   );
 
-  const filteredContact = filteredContacts();
+  // const filteredContact = filteredContacts();
   return (
     <Container>
       <MainTitle>Phonebook 📱</MainTitle>
-      <ContactForm onFormSubmit={onFormSubmit} btnText="Create" />
+      <ContactForm />
       <Title>Contacts 📑</Title>
-      <Filter onInputChange={onInputChange} />
-      <ContactList data={filteredContact} deleteContact={deleteContact} />
+      <Filter />
+      <ContactList />
       <GlobalStyleComponent />
     </Container>
   );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App/App';
 import 'modern-normalize/modern-normalize.css';
-// import './index.css';
+
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -10,9 +10,10 @@ import { persistor } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider srore={store}>
-      <PersistGate loading={null} persistor={persistor}></PersistGate>
-      <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
