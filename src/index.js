b9b4803002e96c72cd9f472e7e839d0f21b211fd
Alexon7 <alenkaalexon@gmail.com>
2023-06-07 19:45:54 +0200

    fix provader

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App/App';
 import 'modern-normalize/modern-normalize.css';
 
-import { store } from './redux/store';
+import { store } from 'redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { persistor } from './redux/store';
+import { persistor } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
